2e7af19d1bb6063167e08aa436de3690fed4d0e3
Jakub Mazur <kubam0906@gmail.com>
2025-06-06 20:56:35 +0200

    more generic default value for Lobby

--- Source/MultiplayerSessions/Public/MenuWidget.h
+++ Source/MultiplayerSessions/Public/MenuWidget.h
@@ -19,7 +19,7 @@ class MULTIPLAYERSESSIONS_API UMenuWidget : public UUserWidget
 
 public:
 	UFUNCTION(BlueprintCallable)
-	void MenuSetup(int32 NumberOfPublicConnections = 4, FString TypeOfMatch = FString(TEXT("FreeForAll")), FString LobbyPath = FString(TEXT("/Game/ThirdPerson/Maps/Lobby")));
+	void MenuSetup(int32 NumberOfPublicConnections = 4, FString TypeOfMatch = FString(TEXT("FreeForAll")), FString LobbyPath = FString(TEXT("/Game/Maps/Lobby")));
 
 protected:
 	virtual bool Initialize() override;
